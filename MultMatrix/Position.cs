94566f49a60a64c9c6113d986ca4ae6d9a31a41e
Inna Fisyuk <inna.fisyuk.0203@gmail.com>
2023-04-30 11:44:36 +0300

    Добавьте файлы проекта.

---
+++ MultMatrix/Position.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultMatrix
+{
+    class Position
+    {
+        private int n;
+
+        public int N
+        {
+            get { return n; }
+            set { n = value; }
+        }
+
+        private int k;
+
+        public int K
+        {
+            get { return k; }
+            set { k = value; }
+        }
+
+
+        public Position(int n, int k)
+        {
+            N = n;
+            K = k;
+        }
+
+    }
+}
